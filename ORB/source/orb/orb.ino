cf0bd78062f1765d199dc1302331f0e850197957
alshapton <alshapton@gmail.com>
2023-05-05 09:49:30 +0100

    Tidied up and added additional Project ORB documents + more KiCAD updates

---
+++ ORB/source/orb/orb.ino
@@ -0,0 +1,38 @@
+// 
+//.          ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄  
+//.         ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░▌ 
+//.         ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌
+//.         ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌
+//.         ▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌
+//.         ▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░▌ 
+//.         ▐░▌       ▐░▌▐░█▀▀▀▀█░█▀▀ ▐░█▀▀▀▀▀▀▀█░▌
+//.         ▐░▌       ▐░▌▐░▌     ▐░▌  ▐░▌       ▐░▌
+//.         ▐░█▄▄▄▄▄▄▄█░▌▐░▌      ▐░▌ ▐░█▄▄▄▄▄▄▄█░▌
+//.         ▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░░░░░░░░░▌ 
+// .         ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀  ▀▀▀▀▀▀▀▀▀▀  
+// 
+
+//          Project ORB 
+//.                      "Optional ROM Board"
+//
+//          An Arduino-based emulator for the 
+//          4 x Intel 1702A TTL EPROM chips 
+//          and the "OS" for the SPHERE-1 Computer
+//
+//          Author:       Andrew Shapton
+//
+//          Credits:      Ben Zott for the ROM image binary
+//
+
+
+#include "pds-v3n.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
